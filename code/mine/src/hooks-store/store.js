9cbe60af0279ba41dd3a3f6a44593055de78702c
Sohail Shaukat Selod <sohail47k@gmail.com>
2021-10-05 14:21:24 +0530

    sync

---
+++ code/mine/src/hooks-store/store.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+let gloabalState = {};
+let listeners = [];
+let actions = {};
+
+const useStore = () => {
+    const [] = useState(gloabalState);
+}
---
+++ code/mine/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { combineReducers, createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import ProductsProvider from './context/products-context'
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>,
+  document.getElementById('root')
+);
